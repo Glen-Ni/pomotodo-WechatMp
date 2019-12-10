f2b2d06f933749d56df3c504a5f6dfd916a73d86
Ni Jialun <nijialun888@hotmail.com>
2019-12-10 17:10:54 +0800

    test

--- pages/test/test.js
+++ pages/test/test.js
@@ -5,9 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    message:"我是第一条信息",
+    number:1,
+    str:"我是小僵尸",
+    arr:[1,2,3,4],
+    obj:{
+      id:1,
+      text:"我是小僵尸1"
+    },
+    flag:false
   },
+  pushItem: function(){
+    // var tempArr = this.data.arr
+    // console.log("rua",tempArr)
+    // tempArr.push(tempArr.length+1)
+    this.data.arr.push(this.data.arr.length+1)
+    this.setData({ arr: this.data.arr})
+  },
+  toggleColor: function(){
+    this.setData({flag: !this.data.flag})
+  },
+  reverseStr: function(){
+    this.setData({str:this.data.str.split('').reverse().join('')})
+  },
+  changeObjText: function(){
 
+    var textNum = parseInt(this.data.obj.text.substr(5))+1
+    // console.log(textNum)
+    var textStr = this.data.obj.text.substr(0, 5)
+    console.log(textStr) 
+    this.setData({"obj.text":textStr+textNum })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
