76e9002920d9babd2f43c76424abbe8274e840b5
GlenNi <nijialun888@hotmail.com>
2020-01-29 23:16:34 +0800

    modify button component

--- components/_button/_button.js
+++ components/_button/_button.js
@@ -1,3 +1,17 @@
 // components/_button/_button.js
 Component({
+  properties:{
+    size: {
+      type: String,
+      value:""
+    },
+    text: {
+      type: String,
+      value:""
+    },
+    type: {
+      type: String,
+      value: ""
+    }
+  }
 })
